e2d598dab0f66801bad94ebe2c42afd72983e3f1
trinhthai1613-ai <trinhthai1613@gmail.com>
2025-10-25 12:44:39 +0700

    Update HomeServlet.java

--- WebApplication1/src/java/com/company/leavemgmt/web/HomeServlet.java
+++ WebApplication1/src/java/com/company/leavemgmt/web/HomeServlet.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.company.leavemgmt.web;
 
-/**
- *
- * @author hp
- */
-public class HomeServlet {
-    
+import com.company.leavemgmt.model.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("u", (User) req.getSession().getAttribute("user"));
+        req.getRequestDispatcher("/WEB-INF/jsp/home.jsp").forward(req, resp);
+    }
 }
